aac33cfc10cb0a69054b63c93a168f8ea9817746
JulianskyMe <juliakamynskaya@gmail.com>
2023-12-29 22:11:30 +0200

    Commit 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,53 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryItemsMarkup = galleryItems
+  .map((item) => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>`;
+  })
+  .join("");
+
+const galleryElem = document.querySelector("ul.gallery");
+
+galleryElem.innerHTML = galleryItemsMarkup;
+galleryElem.addEventListener("click", selectItem);
+
+function selectItem(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: (instance) => {
+        document.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscKeyPress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
+
+const iframe = document.querySelector('iframe');
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
